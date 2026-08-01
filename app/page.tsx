5177d9aa6dc0fbbb6071a4494386eb8a14b921b2
xkumailx <kumail.bmj@gmail.com>
2026-08-01 11:38:10 +0500

    fixing after live

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default async function HomePage() {
       <Section>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           <ExperienceCards
-            href="Dining.pdf"
+            href="Food-Menu.pdf"
             image="/View-food-menu.png"
             imageAlt="Our Menu"
             eyebrow="Modern Japanese"
